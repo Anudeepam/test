2a775e46398fce3dda9def8fc9bef74bbbe916a7
Anudeepam <47081655+Anudeepam@users.noreply.github.com>
2021-07-02 12:12:26 +0530

    Add files via upload

---
+++ err.js
@@ -0,0 +1,19 @@
+const prompts = [
+["resolution for ab11"],
+["resolution for aoao"]
+]
+
+const replies = [
+["Step 1:  Open abend Ab11. You can find the same in Sarview. This will open the abend AB11. Then proceed with resolving.\nStep 2: Try to resolve abend\nStep 3: Done\n"],
+["Step 1: \nStep 2: \nStep 3:\n"]
+]
+
+const alternative = [
+"Same",
+"Go on...",
+"Bro...",
+"Try again",
+"I'm listening...",
+"I don't understand :/"]
+
+const coronavirus = ["Please stay home","Wear a mask"]
